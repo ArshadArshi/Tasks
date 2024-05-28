13b572b1835a49bfe92180cdb644d5bc10d2469b
ArshadArshi <arshadmubarak17@gmail.com>
2024-05-28 12:26:51 +0530

    fixed styling

---
+++ src/Component/About.js
@@ -0,0 +1,28 @@
+import './All.css'
+import arshi from './arshi.jpg'
+
+const About = () => {
+    return (
+      <div className='about'>
+          <img src={arshi} alt="Arshi" width='500px' height='600vh'/>
+          <div className="para">
+          <p>Arshad Mubarak Mohammed known as the React Developer once was a very cool and compressed🇬🇾
+              because of the some circumstances he has changed a lot following his dreams for making his
+              parents feel proud 
+          </p>
+          <p>
+              He is a good influencer of the life by giving the motivational quotes about the life how
+              to overcome from depreeion and move on like a missile and how to train our selves to become successful
+              in life 
+          </p>
+          <p>He is a love failure and becaus eof that past he has now become the most famous and popular 
+              by making himself as a unforgettable person in the world 
+          </p>
+          <p>Never beg anyone for anything because everyone has their own problem so that no one will "HELP" you
+              <span>-ARSHAD</span>
+          </p>
+          </div>
+      </div>
+    )
+  }
+  export default About
